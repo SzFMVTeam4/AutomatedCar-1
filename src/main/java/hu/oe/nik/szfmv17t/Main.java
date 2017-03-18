d9984130aca99a4c6a302fbd7162bdd8fdb9aee4
ZalanMariusz <zalan.mariusz@gmail.com>
2017-03-18 09:34:53 +0100

    Test code for drawing, and some setup code
    -main wind-ow resizes at the start according to the display resolution
    -drawing implemented on a different threat
    -drawing test code
    -player image replaced
    -etc...

--- src/main/java/hu/oe/nik/szfmv17t/Main.java
+++ src/main/java/hu/oe/nik/szfmv17t/Main.java
@@ -1,8 +1,10 @@
 package hu.oe.nik.szfmv17t;
 
 import hu.oe.nik.szfmv17t.automatedcar.AutomatedCar;
-import hu.oe.nik.szfmv17t.environment.World;
+import hu.oe.nik.szfmv17t.automatedcar.hmi.HMI;
+import hu.oe.nik.szfmv17t.environment.domain.World;
 import hu.oe.nik.szfmv17t.visualisation.CourseDisplay;
+import hu.oe.nik.szfmv17t.visualisation.HmiJPanel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,16 +14,25 @@ public class Main {
 	private static final int CYCLE_PERIOD = 200;
 	public static final int worldHeight = 600;
 	public static final int worldWidth= 800;
-	public static final int FPS=30;
+	public static final int FPS=60;
 	public static void main(String[] args) throws InterruptedException {
 		CourseDisplay vis = new CourseDisplay();
 		//not a pun
 		// create the world
-		World w = new World(800,600);
+		World w = new World("src/main/resources/test_world.xml");
+		// create an automated car NEW signature
+		AutomatedCar car = new AutomatedCar(300,300,100d,100d,90d,0,"car_1_white.png",100d,0d,0d);
+
 		// create an automated car
-		AutomatedCar car = new AutomatedCar(20,20, "bosch1.png");
+
+		//create HMI - Human machine interface
+		HMI hmi = new HMI();
+		HmiJPanel.setHmi(hmi);
+
+
 		// add car to the world
 		w.addObjectToWorld(car);
+
 		// init visualisation module with the world
 		vis.init(w);
 
