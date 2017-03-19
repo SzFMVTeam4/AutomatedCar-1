06876bb9db4c8dd791981d358e9960644a8765d5
shepherd1227 <shepherd1227@gmail.com>
2017-03-19 23:14:44 +0100

    Changed Visualisation Team's Code - use the new "getWorld()" method as the UML shows

--- src/main/java/hu/oe/nik/szfmv17t/visualisation/CourseDisplay.java
+++ src/main/java/hu/oe/nik/szfmv17t/visualisation/CourseDisplay.java
@@ -1,90 +1,71 @@
 package hu.oe.nik.szfmv17t.visualisation;
 
-import hu.oe.nik.szfmv17t.Main;
 import hu.oe.nik.szfmv17t.environment.domain.World;
-import hu.oe.nik.szfmv17t.environment.interfaces.IWorld;
 import hu.oe.nik.szfmv17t.environment.interfaces.IWorldObject;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferStrategy;
+import java.awt.BorderLayout;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-import hu.oe.nik.szfmv17t.environment.interfaces.IWorldVisualisation;
-import hu.oe.nik.szfmv17t.environment.utils.Config;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-public class CourseDisplay implements Runnable{
+public class CourseDisplay {
 
 	private static final Logger logger = LogManager.getLogger();
 	private JFrame frame = new JFrame("OE NIK Automated Car Project");
 	private JPanel hmiJPanel;
-	private JPanel mainPanel;
-	private JPanel worldObjectsJPanel;
-	//private Drawer drawer;
-	private World world;
-	private BufferStrategy strategy;
 
 	public void refreshFrame() {
 		frame.invalidate();
 		hmiJPanel.invalidate();
-		//mainPanel.invalidate();
-		//frame.pack();
 		frame.validate();
 		frame.repaint();
 	}
 
 	public void init(World world){
-		this.world=world;
-
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		mainPanel = new JPanel();
+		JPanel mainPanel = new JPanel();
 		mainPanel.setLayout(new BorderLayout());
-		frame.setVisible(true);
-		frame.createBufferStrategy(4);
-		strategy = frame.getBufferStrategy();
-		worldObjectsJPanel = new JPanel() {
+
+		JPanel worldObjectsJPanel = new JPanel() {
 					  private static final long serialVersionUID = 1L;
+
 					  public void paintComponent(Graphics g) {
+
 						  for (IWorldObject object : world.getWorld()) {
 							  // draw objects
 							  BufferedImage image;
-
-							  g = strategy.getDrawGraphics();
-							  Graphics2D g2d=(Graphics2D)g.create();
 							  try {
 								  image = ImageIO.read(new File(ClassLoader.getSystemResource(object.getImageName()).getFile()));
-                                                                  int segedx=((int)(object.getCenterX()-object.getWidth()/2));
-                                                                  int segedy=((int)(object.getCenterY()-object.getHeight()/2));
-         								  g2d.drawImage(image,
-												  segedx, segedy, null);
+                                                                  int segedx=((int)(object.getCenterX()+0.5d));
+                                                                  int segedy=((int)(object.getCenterY()+0.5d));
+								  g.drawImage(image,segedx, segedy, null);
 							  } catch (IOException e) {
 								  logger.error(e.getMessage());
 							  }
-							  g2d.dispose();
-							  g.dispose();
 						  }
 					  }
 				  };
+
 		mainPanel.add(worldObjectsJPanel,BorderLayout.CENTER);
 		hmiJPanel = getSmiJPanel();
 		mainPanel.add(hmiJPanel, BorderLayout.SOUTH);
-		SizeFrame(frame);
+
 		//Solve the duplicated key listener
 		//addSmiKeyEventListenerToFrame();
+
+		frame.setSize(world.getWidth(), world.getHeight());
 		frame.add(mainPanel);
 		frame.validate();
-		//frame.setVisible(true);
-		frame.setResizable(false);
+		frame.setVisible(true);
 	}
 
 	public JPanel getSmiJPanel() {
@@ -100,40 +81,6 @@ public class CourseDisplay implements Runnable{
 			frame.addKeyListener(HmiJPanel.getHmi());
 		}else{
 			logger.error("JFrame frame or HmiJPanel.getHmi() returned null");
-		}
-	}
-
-	private void SizeFrame(JFrame frame)
-	{
-		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-		int width = gd.getDisplayMode().getWidth();
-		int height = gd.getDisplayMode().getHeight();
-		frame.setSize(Config.getScreenWidth,Config.getScreenHeight- 50);
-	}
-	private JPanel filler()
-	{
-		JPanel filler=new JPanel();
-		filler.setOpaque(true);
-		filler.setBackground(Color.orange);
-		filler.setPreferredSize(new Dimension(200, 40));
-		return filler;
-	}
-	@Override
-	public void run() {
-		int refreshRate = 1000 / Config.FPS;
-		while (true)
-		{
-			try {
-				Thread.sleep(refreshRate);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			try {
-				Drawer.getDrawer(world).DrawFrametoPanel(worldObjectsJPanel,world,mainPanel);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			refreshFrame();
 		}
 	}
 }
