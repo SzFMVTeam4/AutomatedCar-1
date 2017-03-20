569881c86140964747478c463a886d8d05a8b610
dlac03 <dlac03@gmail.com>
2017-03-20 21:31:38 +0100

    drawer fixes

--- src/main/java/hu/oe/nik/szfmv17t/visualisation/Drawer.java
+++ src/main/java/hu/oe/nik/szfmv17t/visualisation/Drawer.java
@@ -62,25 +62,29 @@ public class Drawer implements IWorldVisualization {
 
         BorderLayout layout = (BorderLayout)mainPanel.getLayout();
         mainPanel.remove(layout.getLayoutComponent(BorderLayout.CENTER));;
-        List<IWorldObject> toDraw=getComposer(world).composeFrame();
+        FrameComposer fc = getComposer(world);
+        fc.setCameraSize(worldObjectsPanel.getWidth(),worldObjectsPanel.getHeight());
+        List<IWorldObject> toDraw=fc.composeFrame();
 
         worldObjectsPanel = new JPanel() {
             private static final long serialVersionUID = 1L;
             public void paintComponent(Graphics g) {
                 int t2=0;
                 BufferedImage image;
+                Graphics2D g2d=(Graphics2D)g.create();
                 for (IWorldObject object : toDraw) {
                     // draw objects
                     image = worldImages.get(t2++);
-                    Graphics2D g2d=(Graphics2D)g.create();
-                    g2d.rotate(object.getAxisAngle()+Math.PI/2,object.getCenterX(),object.getCenterY());
                     int segedx=((int)(object.getCenterX()-object.getWidth()/2));
-                    int segedy=((int)(object.getCenterY()-object.getHeight()/2)+t);
+                    int segedy=((int)(object.getCenterY()-object.getHeight()/2));
                     //DEBUG OVERLAY
                     PutDebugInformationOnImage(image, object);
-                    g2d.drawImage(image, segedx, segedy, null);
-                    g2d.dispose();
+                    AffineTransform transform = new AffineTransform();
+                    transform.translate(segedx, segedy);
+                    transform.rotate(object.getAxisAngle()+Math.PI/2);
+                    g2d.drawImage(image,transform, null);
                 }
+                g2d.dispose();
                 t+=5;
             }
         };
